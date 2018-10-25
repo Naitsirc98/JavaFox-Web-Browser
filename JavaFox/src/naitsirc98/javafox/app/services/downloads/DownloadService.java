515b952282350e763630754e09a29d1d10c369ec
Cristian Herrera <naitsirc98@gmail.com>
2018-10-25 01:03:15 +0100

    Minor changes

--- JavaFox/src/naitsirc98/javafox/app/services/downloads/DownloadService.java
+++ JavaFox/src/naitsirc98/javafox/app/services/downloads/DownloadService.java
@@ -6,19 +6,21 @@ import java.net.URL;
 import java.nio.channels.Channels;
 import java.nio.channels.FileChannel;
 import java.nio.channels.ReadableByteChannel;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javafx.concurrent.Service;
 import javafx.concurrent.Task;
 import naitsirc98.javafox.app.user.config.UserConfig;
 
 public final class DownloadService extends Service<Void> {
-	
+
 	private final DownloadProgressService downloadProgress;
-	
+
 	private final String url;
 	private final String filename;
 	private final double size;
-	
+
 	public DownloadService(String url, String filename, double size) {
 		this.url = url;
 		this.filename = filename;
@@ -27,11 +29,11 @@ public final class DownloadService extends Service<Void> {
 		setOnCancelled(e -> System.out.println("Download cancelled"));
 		setOnSucceeded(e -> System.out.println("Download complete"));
 	}
-	
+
 	public DownloadProgressService getProgressService() {
 		return downloadProgress;
 	}
-	
+
 	@Override
 	public boolean cancel() {
 		if(downloadProgress != null) {
@@ -39,14 +41,14 @@ public final class DownloadService extends Service<Void> {
 		}
 		return super.cancel();
 	}
-	
+
 	@Override
 	protected Task<Void> createTask() {
 		return new DownloadTask();
 	}
-	
+
 	private class DownloadTask extends Task<Void> {
-		
+
 		public DownloadTask() {
 			setOnScheduled(e -> updateMessage("Preparing for download..."));
 			setOnCancelled(e -> updateMessage("Download cancelled"));
@@ -54,30 +56,27 @@ public final class DownloadService extends Service<Void> {
 			setOnSucceeded(e -> updateMessage("Download complete"));
 		}
 
+
 		@Override
 		protected Void call() throws Exception {
-			
+
 			downloadProgress.messageProperty().addListener((observable, old, neww) -> {
 				updateMessage(neww);
 			});
+
+			final String path = UserConfig.getConfig().getString("downloads").concat(filename);
 			
-			System.out.println("downloading = "+filename);
-			
-			final String path = UserConfig.getConfig().getString("downloads")+filename;
-			
+			System.out.println("Downloading ".concat(filename));
+
 			try(final ReadableByteChannel reader 
 					= Channels.newChannel(new URL(url).openStream());
-					FileOutputStream file = new FileOutputStream(path)) {
-				
-				FileChannel fileChannel = file.getChannel();
-				
+					final FileChannel fileChannel 
+					= new FileOutputStream(path).getChannel();) {
+
 				downloadProgress.start(path, size);
-				
-				fileChannel.transferFrom(reader, 0, Long.MAX_VALUE);
-				
-				fileChannel.close();
-				
-				
+
+				fileChannel.transferFrom(reader, 0L, Long.MAX_VALUE);
+
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
@@ -87,8 +86,8 @@ public final class DownloadService extends Service<Void> {
 
 	}
 
-	
-	
+
+
 
 
 
